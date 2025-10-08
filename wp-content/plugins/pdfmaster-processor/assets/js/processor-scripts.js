cd9bc7e6d152ee018e9c29228063b8e23dc769c5
zurychhh <134611895+zurychhh@users.noreply.github.com>
2025-10-08 10:51:04 +0200

    feat(ui): show download button after processing\n\n- Hide form on success and render download link with  attribute\n- Add "Process Another File" reset button to restore form\n- Basic alerts for error cases

--- wp-content/plugins/pdfmaster-processor/assets/js/processor-scripts.js
+++ wp-content/plugins/pdfmaster-processor/assets/js/processor-scripts.js
@@ -4,6 +4,8 @@
     $(document).on('submit', '.pdfm-processor__form', function (event) {
         event.preventDefault();
 
+        const $form = $(this);
+        const $container = $form.closest('.pdfm-processor');
         const formData = new FormData(this);
         formData.append('action', 'pdfm_process_pdf');
         formData.append('nonce', pdfmProcessor.nonce);
@@ -16,11 +18,37 @@
             contentType: false,
         })
             .done(function (response) {
-                // TODO: Render success state using Elementor widgets.
-                console.log('PDF processing response', response);
+                if (response && response.success && response.data && response.data.url) {
+                    const url = response.data.url;
+                    // Hide form and show download UI
+                    $form.hide();
+                    const resultHtml = [
+                        '<div class="pdfm-result">',
+                        '<p>Success! Your file is ready.</p>',
+                        '<a class="pdfm-download button button-primary" href="' + url + '" download>Download Your Compressed PDF</a> ',
+                        '<button type="button" class="pdfm-reset button button-secondary">Process Another File</button>',
+                        '</div>'
+                    ].join('');
+                    $container.append(resultHtml);
+                } else {
+                    const message = (response && response.data && response.data.message) ? response.data.message : 'Processing failed.';
+                    alert(message);
+                }
             })
             .fail(function (error) {
                 console.error('PDF processing error', error);
+                alert('An error occurred while processing the PDF.');
             });
+    });
+
+    // Reset handler to allow another processing run
+    $(document).on('click', '.pdfm-reset', function () {
+        const $container = $(this).closest('.pdfm-processor');
+        const $form = $container.find('.pdfm-processor__form');
+        $container.find('.pdfm-result').remove();
+        if ($form.length) {
+            $form[0].reset();
+            $form.show();
+        }
     });
 })(jQuery);
